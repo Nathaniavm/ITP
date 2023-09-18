587b3f370ddc033e2dc66a5348ba45ab27ccd7ce
Kleinc <kleinc@stud.ntnu.no>
2023-09-18 12:49:38 +0200

    Fikser konstruktoren i Profile

--- bankapp/bankapp-core/src/main/java/bankapp/core/BankCard.java
+++ bankapp/bankapp-core/src/main/java/bankapp/core/BankCard.java
@@ -1,25 +1,14 @@
 package bankapp.core;
 
 public class BankCard {
-    private int cardNr;
     private String cardholder;
-    
-    
-    public BankCard(int cardNr, String cardholder) {
-        //Fiks logikk for hvordan kortnummer skal se ut, og navn som er tillat og sånt
-        this.cardNr = cardNr;
+
+    public BankCard(String cardholder) {
         this.cardholder = cardholder;
     }
-
-
-    public int getCardNr() {
-        return cardNr;
-    }
-
 
     public String getCardholder() {
         return cardholder;
     }
 
-    
 }
