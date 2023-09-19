47d826d9d37b2dd865e3f5e51cc02325da400da0
Alice <alicezheng888@gmail.com>
2023-09-19 12:20:24 +0200

    skrev kode for å sjekke om email er gyldig

--- bankapp/bankapp-core/src/main/java/bankapp/core/Profile.java
+++ bankapp/bankapp-core/src/main/java/bankapp/core/Profile.java
@@ -1,6 +1,7 @@
 package bankapp.core;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Profile {
@@ -9,6 +10,21 @@ public class Profile {
     private String tlf;
     private String password;
     private List<Account> accounts = new ArrayList<>();
+    private ArrayList<String> landskoder = new ArrayList<>(Arrays.asList("ad", "ae", "af", "ag", "ai", "al", "am", "ao",
+            "aq", "ar", "as", "at", "au", "aw", "ax", "az", "ba", "bb", "bd", "be", "bf", "bg", "bh", "bi", "bj", "bl",
+            "bm", "bn", "bo", "bq", "br", "bs", "bt", "bv", "bw", "by", "bz", "ca", "cc", "cd", "cf", "cg", "ch", "ci",
+            "ck", "cl", "cm", "cn", "co", "cr", "cu", "cv", "cw", "cx", "cy", "cz", "de", "dj", "dk", "dm", "do", "dz",
+            "ec", "ee", "eg", "eh", "er", "es", "et", "fi", "fj", "fk", "fm", "fo", "fr", "ga", "gb", "gd", "ge", "gf",
+            "gg", "gh", "gi", "gl", "gm", "gn", "gp", "gq", "gr", "gs", "gt", "gu", "gw", "gy", "hk", "hm", "hn", "hr",
+            "ht", "hu", "id", "ie", "il", "im", "in", "io", "iq", "ir", "is", "it", "je", "jm", "jo", "jp", "ke", "kg",
+            "kh", "ki", "km", "kn", "kp", "kr", "kw", "ky", "kz", "la", "lb", "lc", "li", "lk", "lr", "ls", "lt", "lu",
+            "lv", "ly", "ma", "mc", "md", "me", "mf", "mg", "mh", "mk", "ml", "mm", "mn", "mo", "mp", "mq", "mr", "ms",
+            "mt", "mu", "mv", "mw", "mx", "my", "mz", "na", "nc", "ne", "nf", "ng", "ni", "nl", "no", "np", "nr", "nu",
+            "nz", "om", "pa", "pe", "pf", "pg", "ph", "pk", "pl", "pm", "pn", "pr", "ps", "pt", "pw", "py", "qa", "re",
+            "ro", "rs", "ru", "rw", "sa", "sb", "sc", "sd", "se", "sg", "sh", "si", "sj", "sk", "sl", "sm", "sn", "so",
+            "sr", "ss", "st", "sv", "sx", "sy", "sz", "tc", "td", "tf", "tg", "th", "tj", "tk", "tl", "tm", "tn", "to",
+            "tr", "tt", "tv", "tw", "tz", "ua", "ug", "um", "us", "uy", "uz", "va", "vc", "ve", "vg", "vi", "vn", "vu",
+            "wf", "ws", "ye", "yt", "za", "zm", "zw"));
 
     public Profile(String name, String email, String tlf, String password) {
         if ((name.contains(" "))) {
@@ -50,6 +66,28 @@ public class Profile {
 
         catch (NumberFormatException e) {
             return false;
+        }
+    }
+
+    public boolean validEmail(String email) {
+        String[] splitEmail = email.split("\\.", 5);
+        if (splitEmail.length != 3) {
+            throw new IllegalArgumentException("Dette er en ugyldig verdi");
+        }
+        String firstName = getName().split(" ")[0];
+        String lastName = getName().split(" ")[1];
+
+        if (!splitEmail[0].toLowerCase().equals(firstName.toLowerCase())
+                || !splitEmail[1].toLowerCase().subSequence(0, lastName.length()).equals(lastName.toLowerCase())) {
+            throw new IllegalArgumentException("Dette er en ugyldig verdi");
+        } else if (!splitEmail[1].substring(lastName.length(), lastName.length() + 1).equals("@")
+        // || !splitEmail[1].substring(lastName.length() + 1).equals("ntnu")
+        ) {
+            throw new IllegalArgumentException("Dette er en ugyldig verdi");
+        } else if (!landskoder.contains(splitEmail[2])) {
+            throw new IllegalArgumentException("Dette er en ugyldig verdi");
+        } else {
+            return true;
         }
     }
 
