b7a6d878b1d759feb69d0187e1751569daf897fb
Alice <alicezheng888@gmail.com>
2023-10-05 14:42:45 +0200

    lagt til modules, og endret pom slik at den kjører korrekt

---
+++ bankapp/core/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module bankapp.core {
+
+    requires com.fasterxml.jackson.annotation;
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
+    requires junit;
+
+    exports bankapp.core;
+}
