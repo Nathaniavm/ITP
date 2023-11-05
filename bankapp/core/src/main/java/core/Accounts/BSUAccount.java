8cf04ab859168598df48c82463adedd6a5582570
Kleinc <kleinc@stud.ntnu.no>
2023-11-05 14:18:09 +0100

    Ryddet i alle klasser i core i henhold til Checkstyle og Spotbugs. Co-Author-By: @philipvl

--- bankapp/core/src/main/java/core/Accounts/BSUAccount.java
+++ bankapp/core/src/main/java/core/Accounts/BSUAccount.java
@@ -1,24 +1,23 @@
 package core.Accounts;
 
-import java.io.Serializable;
-
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
-
 import core.Profile;
+import java.io.Serializable;
 
 /**
- * Creates a new BSU-Account to the given profile, with a given name
+ * Creates a new BSU-Account to the given profile, with a given name.
  */
 @JsonIdentityInfo(generator = ObjectIdGenerators.UUIDGenerator.class, property = "@id")
 public class BSUAccount extends AbstractAccount implements Serializable {
 
   /**
-   * Creates a new BSU-Account
+   * Creates a new BSU-Account.
    * 
    * @param name    The name of the account
    * @param profile The profile to make an account for
+   * 
    */
   public BSUAccount(@JsonProperty("name") String name, @JsonProperty("profile") Profile profile) {
     super(name, profile);
